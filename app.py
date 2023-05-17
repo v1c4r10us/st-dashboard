deca122981c54d8afe9978610fcfe44628f5b62d
v1c4r10us <v1c4r10us.29@gmail.com>
2023-05-17 05:23:14 -0500

    adding controls

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ st.pydeck_chart(pdk.Deck(
             'ScatterplotLayer',
             data=df,
             get_position='[lon, lat]',
-            get_color='[200, 30, 0, 160]',
+            get_color='[85, 0, 66, 22]',
             get_radius=16000,
         ),
     ],
