342e1a73ca23d56a3d7393a2c140503bdcf531d5
v1c4r10us <v1c4r10us.29@gmail.com>
2023-05-17 05:14:32 -0500

    adding controls

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ st.pydeck_chart(pdk.Deck(
            'HexagonLayer',
            data=df,
            get_position='[lon, lat]',
-           radius=200,
+           radius=2000,
            elevation_scale=4,
            elevation_range=[0, 1000],
            pickable=True,
@@ -60,7 +60,7 @@ st.pydeck_chart(pdk.Deck(
             data=df,
             get_position='[lon, lat]',
             get_color='[200, 30, 0, 160]',
-            get_radius=200,
+            get_radius=4000,
         ),
     ],
 ))
